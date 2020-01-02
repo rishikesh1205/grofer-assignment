9dead08f56e500b40599c4c9014dab02400206bc
Rishikesh <rishikeshkrishnan4567@gmail.com>
2020-01-02 23:32:31 +0530

    Integrated with Mongo Atlas

--- model/index.js
+++ model/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Product = require('./Product');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerceDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://root:rishikesh@cluster1-ysedy.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
 
 const Schema = mongoose.Schema;
