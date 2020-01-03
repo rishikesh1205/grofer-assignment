239a4a84e065d922cbfbbf8663f3b40d910a932d
Rishikesh <rishikeshkrishnan4567@gmail.com>
2020-01-03 12:57:14 +0530

    MongoDB Atlas Connection configured

--- model/index.js
+++ model/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const Product = require('./Product');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://root:rishikesh@cluster1-ysedy.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://root:rishikesh@cluster1-ysedy.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+    .then(() => { console.log("Database started") },
+        (error) => console.log(error));
 mongoose.set('useFindAndModify', false);
-
 const Schema = mongoose.Schema;
-
 const db = {
-    Product : Product(mongoose , Schema),
+    Product: Product(mongoose, Schema),
 }
 module.exports = db;
